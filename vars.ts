e536a6acbd6357010c5083648fe77d0ec629dae4
dallinmoak <dallinmoak@gmail.com>
2018-03-12 12:18:56 -0600

    things

--- vars.ts
+++ vars.ts
@@ -1,14 +1,17 @@
 // var fullName : string = "Dallin Moak";
 // let paidAccount : boolean = true;
 // const versionNumber : number = 1.3;
+
 // fullName = "asdf";
 // paidAccount = false;
+
 // console.log(fullName);
 // console.log(paidAccount);
 // console.log(versionNumber);
+
 function printName(f, l) {
-    var greeting = "hi there, ";
-    console.log(greeting + f + " " + l);
+  var greeting : string = "hi there, ";
+  console.log(greeting + f + " " + l);
 }
+
 printName("Dallin", "Moak");
-//# sourceMappingURL=vars.js.map
