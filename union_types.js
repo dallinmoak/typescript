e6165278918fe7e2022415238032d5690831b222
dallinmoak <dallinmoak@gmail.com>
2018-03-14 13:28:26 -0600

    attempted union types tutorial (not done)

---
+++ union_types.js
@@ -0,0 +1,6 @@
+// type PlayerArray = Array<string|number>;
+// let players : PlayerArray = ["altuve", "corerra", "bregeman"];
+// let player_numbers : PlayerArray = [25,3,2];
+// console.log(players);
+// console.log(player_numbers);
+//# sourceMappingURL=union_types.js.map
---
+++ union_types.ts
@@ -0,0 +1,5 @@
+// type PlayerArray = Array<string|number>;
+// let players : PlayerArray = ["altuve", "corerra", "bregeman"];
+// let player_numbers : PlayerArray = [25,3,2];
+// console.log(players);
+// console.log(player_numbers);
