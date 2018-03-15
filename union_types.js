f45ea75d8f06a29b142b7c59df09f02bc97e591f
dallinmoak <dallinmoak@gmail.com>
2018-03-15 16:07:22 -0600

    union types

--- union_types.js
+++ union_types.js
@@ -1,6 +1,10 @@
-// type PlayerArray = Array<string|number>;
-// let players : PlayerArray = ["altuve", "corerra", "bregeman"];
-// let player_numbers : PlayerArray = [25,3,2];
-// console.log(players);
-// console.log(player_numbers);
+let players = ["altuve", "corerra", "bregeman"];
+let player_numbers = [25, 3, 2];
+console.log(players);
+console.log(player_numbers);
+var names;
+names = ["dallin", "joe"];
+console.log(names);
+names = "jack";
+console.log(names);
 //# sourceMappingURL=union_types.js.map
--- union_types.ts
+++ union_types.ts
@@ -1,5 +1,11 @@
-// type PlayerArray = Array<string|number>;
-// let players : PlayerArray = ["altuve", "corerra", "bregeman"];
-// let player_numbers : PlayerArray = [25,3,2];
-// console.log(players);
-// console.log(player_numbers);
+type PlayerArray = Array<string|number>;
+let players : PlayerArray = ["altuve", "corerra", "bregeman"];
+let player_numbers : PlayerArray = [25,3,2];
+console.log(players);
+console.log(player_numbers);
+
+var names : string[]|string;
+names = ["dallin", "joe"];
+console.log(names);
+names = "jack";
+console.log(names);
